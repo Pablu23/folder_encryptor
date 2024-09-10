349e2ca8e3d3e93eed40a6a279f86989b0f1f3e5
Pablu23 <pablu23@gmx.de>
2024-09-10 10:46:57 +0200

    Improve borrows and ownership

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use chacha20::XChaCha20;
 use core::time;
 use rand::RngCore;
 use rand_core::OsRng;
-use std::hint;
 use std::path::{Path, PathBuf};
 use std::sync::atomic::AtomicUsize;
 use std::{
@@ -18,7 +17,10 @@ use std::{
     thread,
     thread::{available_parallelism, JoinHandle},
 };
+use std::{hint, panic};
 use uuid::Uuid;
+
+mod threadpool;
 
 extern crate rpassword;
 
@@ -28,9 +30,9 @@ const BUFFER_LEN: usize = 64 * 1024; // 64 KiB
 
 pub fn encrypt_file(
     source_path: String,
-    root_path: &String,
-    pwd: &String,
-    config: &Config,
+    root_path: String,
+    pwd: String,
+    config: Config,
 ) -> Result<()> {
     let mut nonce = [0u8; 24];
     OsRng.fill_bytes(&mut nonce);
@@ -44,7 +46,7 @@ pub fn encrypt_file(
 
     let mut source_file = File::open(&source_path)?;
     let mut dest_file = File::create(dest_path)?;
-    let mut buffer = vec![0u8; BUFFER_LEN]/* .into_boxed_slice() */;
+    let mut buffer = vec![0u8; BUFFER_LEN];
 
     println!("Now encrypting {source_path}");
 
@@ -85,8 +87,9 @@ pub fn encrypt_file(
     Ok(())
 }
 
-pub fn decrypt_file(source_path: &Path, pwd: &String, config: &Config) -> Result<()> {
+pub fn decrypt_file(source_path: PathBuf, pwd: String, config: Config) -> Result<()> {
     let mut nonce = [0u8; 24];
+
     if !source_path.try_exists()? {
         bail!("File not found");
     }
@@ -126,27 +129,28 @@ pub fn decrypt_file(source_path: &Path, pwd: &String, config: &Config) -> Result
     let path = root_dir_path.join(&file_name);
 
     let prefix = path.parent().expect("No parent Directory");
+
     std::fs::create_dir_all(prefix)?;
 
     let mut dest_file = File::create(path)?;
-    let mut buffer = vec![0u8; BUFFER_LEN]/* .into_boxed_slice() */;
+    let mut buffer = vec![0u8; BUFFER_LEN];
     loop {
-        let read_count = source_file.read(&mut buffer)?;
+        let read_count = source_file.read(&mut buffer).unwrap();
 
         if read_count == BUFFER_LEN {
             cipher.apply_keystream(&mut buffer);
-            dest_file.write(&buffer)?;
+            dest_file.write(&buffer).unwrap();
         } else {
             cipher.apply_keystream(&mut buffer[..read_count]);
 
-            dest_file.write(&buffer[..read_count])?;
+            dest_file.write(&buffer[..read_count]).unwrap();
             break;
         }
     }
 
     println!("Finished decrpyting File {file_name}");
-
     fs::remove_file(source_path)?;
+
     Ok(())
 }
 
@@ -170,14 +174,14 @@ fn populate_file_list(
 }
 
 fn main() -> io::Result<()> {
-    let config = Arc::new(argon2::Config {
+    let config = argon2::Config {
         variant: argon2::Variant::Argon2id,
         hash_length: 32,
         lanes: 8,
         mem_cost: 16 * 1024,
         time_cost: 8,
         ..Default::default()
-    });
+    };
 
     let exe = env::current_exe().unwrap();
     let cwd = env::current_dir().unwrap();
@@ -199,13 +203,13 @@ fn main() -> io::Result<()> {
 
         print!("Type password for encrypted files: ");
         std::io::stdout().flush().unwrap();
-        let pwd = Arc::new(read_password().unwrap());
+        let pwd = read_password().unwrap();
 
         let mut handles: Vec<JoinHandle<()>> = Vec::with_capacity(max_threads);
         let curr_threads = Arc::new(AtomicUsize::new(0));
 
-        for path_result in paths {
-            let path = path_result.unwrap().path();
+        for path in paths {
+            let path = path.unwrap().path();
             let pwd = pwd.clone();
             let config = config.clone();
             let curr_threads_clone = curr_threads.clone();
@@ -213,12 +217,7 @@ fn main() -> io::Result<()> {
             handles.push(thread::spawn(move || {
                 curr_threads_clone.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
                 if path.is_file() {
-                    decrypt_file(
-                        path.as_path(),
-                        &pwd,
-                        &config
-                    )
-                    .unwrap();
+                    decrypt_file(path, pwd, config).unwrap();
                 }
 
                 curr_threads_clone.fetch_sub(1, std::sync::atomic::Ordering::SeqCst);
@@ -230,7 +229,7 @@ fn main() -> io::Result<()> {
         }
 
         for thread in handles {
-            let _ = thread.join();
+            thread.join().unwrap();
         }
 
         thread::sleep(time::Duration::from_millis(10));
@@ -260,13 +259,12 @@ fn main() -> io::Result<()> {
 
         print!("Type password to encrypt files: ");
         std::io::stdout().flush().unwrap();
-        let pwd = Arc::new(read_password().unwrap());
-        let exe = Arc::new(exe);
+        let pwd = read_password().unwrap();
+        let exe = exe;
         let mut handles: Vec<JoinHandle<()>> = Vec::with_capacity(max_threads);
-        let cwd: Arc<String> = Arc::from(String::from(cwd.to_str().unwrap()));
+        let cwd = String::from(cwd.to_str().unwrap());
 
         let curr_threads = Arc::new(AtomicUsize::new(0));
-
         for path in paths {
             let pwd = pwd.clone();
             let exe = exe.clone();
@@ -279,9 +277,9 @@ fn main() -> io::Result<()> {
                 if path.is_file() && path.as_os_str() != exe.as_os_str() {
                     encrypt_file(
                         String::from(path.to_str().unwrap()),
-                        &cwd.to_string(),
-                        &pwd.to_string(),
-                        &config,
+                        cwd.to_string(),
+                        pwd.to_string(),
+                        config,
                     )
                     .unwrap();
                 }
